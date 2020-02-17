83766532a368fd36c1881295cf3ba398b2a96e8f
AJ Keeter <aleckeeter94@gmail.com>
2020-02-17 15:25:41 -0500

    Integrates use results hook into application. Creates the filter on API that will display certain results when searched

---
+++ src/hooks/useResults.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import yelp from '../api/Yelp';
+
+export default () => {
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async (searchTerm) => {
+
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'San Juan'
+                }
+            });
+            setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMessage('Something went wrong');
+        }
+    };
+
+    // Call searchApi when component is first rendered
+    // searchApi('pasta');
+    useEffect(() => {
+        searchApi('pasta');
+    }, []);
+
+    return [searchApi, results, errorMessage];
+};
